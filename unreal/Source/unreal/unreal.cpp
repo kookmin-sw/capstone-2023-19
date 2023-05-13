c7d581939d663270f7362bce11ac1c54e70a12e9
jehyeon <hyeon.rot@gmail.com>
2023-05-13 20:16:38 +0900

    unreal 프로젝트 추가

---
+++ unreal/Source/unreal/unreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "unreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal, "unreal" );
